713d570a96a74dc426320defb34a8f14ac8a0f5d
Josh Denzel ng <retroduck385@gmail.com>
2026-09-21 18:50:38 +0800

    added supabase in the env

--- catch/src/supabaseClient.js
+++ catch/src/supabaseClient.js
@@ -1,3 +1,8 @@
 import {createClient} from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
+const {SUPABASE_URL, SUPABASE_ANON_KEY} = import.meta.env
+
+const supabaseUrl = 'SUPABASE_URL'
+const supabaseAnonKey = 'SUPABASE_ANON_KEY'
+
+export const supabase = createClient(supabaseUrl,supabaseAnonKey)
